b591e8b4b846bea04e12b7bbf3fe9bc7fdb843c3
subhash131 <subhashnayak131@gmail.com>
2023-11-10 00:14:39 +0530

    first commit

---
+++ redhat-hackathon/src/test/java/com/example/redhathackathon/RedhatHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redhathackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedhatHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
