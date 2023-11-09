dd0340d3cd9085f4cae59cc015e9e6b380c2c91e
subhash131 <subhashnayak131@gmail.com>
2023-11-10 00:37:54 +0530

    folder structure

--- redhat-hackathon/src/main/java/com/example/redhathackathon/RedhatHackathonApplication.java
+++ redhat-hackathon/src/main/java/com/example/redhathackathon/RedhatHackathonApplication.java
@@ -4,7 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class RedhatHackathonApplication {
+
+	@GetMapping("/")
+	public String getMessage(){
+		return "Spring app";
+	}
+
+	@PostMapping("/{input}")
+	public String getMessage(@pathVariable String input){
+		return "Hi "+ input;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedhatHackathonApplication.class, args);
