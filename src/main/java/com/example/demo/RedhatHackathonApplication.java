bcb99775548a319458140b02d4346d74c006bde1
Subhash Nayak <64570322+subhash131@users.noreply.github.com>
2023-11-14 21:52:05 +0530

    Update RedhatHackathonApplication.java

--- src/main/java/com/example/demo/RedhatHackathonApplication.java
+++ src/main/java/com/example/demo/RedhatHackathonApplication.java
@@ -2,9 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RedhatHackathonApplication {
+	
+	@GetMapping("/")
+	public String getMessage() {
+		return "Hi, Im Alive";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedhatHackathonApplication.class, args);
